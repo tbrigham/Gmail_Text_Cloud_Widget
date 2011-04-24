82f2162b71d75f65aeb700a797f86408dbca477a
Tyler Brigham <tylerscottbrigham@gmail.com>
2011-04-24 16:30:50 -0700

    progressive build -- not implemented -- does not work

--- gen/com/tsb/gmailinboxtextcloud/R.java
+++ gen/com/tsb/gmailinboxtextcloud/R.java
@@ -15,7 +15,16 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int widget_textview=0x7f060000;
+        public static final int textview0=0x7f060000;
+        public static final int textview1=0x7f060001;
+        public static final int textview2=0x7f060002;
+        public static final int textview3=0x7f060003;
+        public static final int textview4=0x7f060004;
+        public static final int textview5=0x7f060005;
+        public static final int textview6=0x7f060006;
+        public static final int textview7=0x7f060007;
+        public static final int textview8=0x7f060008;
+        public static final int textview9=0x7f060009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
