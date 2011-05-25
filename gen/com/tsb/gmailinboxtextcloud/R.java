692839e180543f6c424597b7383a515ac6f36a74
Tyler Brigham <tylerscottbrigham@gmail.com>
2011-05-24 21:54:09 -0700

    New changelist

--- gen/com/tsb/gmailinboxtextcloud/R.java
+++ gen/com/tsb/gmailinboxtextcloud/R.java
@@ -28,9 +28,8 @@ public final class R {
     }
     public static final class layout {
         public static final int gitc_config=0x7f030000;
-        public static final int gitc_preferences=0x7f030001;
-        public static final int gitc_widget_html=0x7f030002;
-        public static final int list_item=0x7f030003;
+        public static final int gitc_widget_html=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class string {
         public static final int alarm=0x7f050002;
@@ -39,6 +38,7 @@ public final class R {
         public static final int widget_text=0x7f050001;
     }
     public static final class xml {
-        public static final int gitc_widget=0x7f040000;
+        public static final int gitc_preferences=0x7f040000;
+        public static final int gitc_widget=0x7f040001;
     }
 }
